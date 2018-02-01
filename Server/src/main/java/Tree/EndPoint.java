923b4473ffdf939a269d676225192563904cc378
Kate Sterpu <sterekaterina@inbox.ru>
2018-02-02 00:16:55 +0400

    Добавлен класс EndPoint( методы как положено не отрегулированы) Есть jetty jersy  и подключен json  в pom

---
+++ Server/src/main/java/Tree/EndPoint.java
@@ -0,0 +1,48 @@
+package Tree;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+
+/**
+ * Created by user on 01.02.2018.
+ */
+@Path("endpoint")
+public class EndPoint {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "hello111";
+    }
+
+    @GET
+    @Path("/alltasks")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getAllTasks() {
+        return "alltasks";
+    }
+
+    @GET
+    @Path("/task")
+    public void getTask() {
+
+    }
+
+    @DELETE
+    @Path("/deletetask")
+    public void deleteTask() {
+
+    }
+
+    @POST
+    @Path("/update")
+    public void updateTask() {
+
+    }
+
+    @POST
+    @Path("/updatetime")
+    public void updateSpentTime() {
+
+    }
+}
